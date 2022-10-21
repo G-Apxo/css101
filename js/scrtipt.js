3da974600424050c052b6334ec45ae624a3b8bc3
Apxo <72552435+G-Apxo@users.noreply.github.com>
2022-10-21 21:02:52 +0400

    Update scrtipt.js

--- js/scrtipt.js
+++ js/scrtipt.js
@@ -92,4 +92,92 @@ var fullname = FirstName + " " + LastName;
 console.log(fullname)
 
 
+var strSum = prompt ("შეიყავნეთ ანაბარის თანხა 1000");
+var strPercent = prompt ("შეიყვანეთ საპროცენტო განაკვეთი");
+
+var sum = parseInt(strSum);
+var percent = parseInt(strPercent);
+
+sum = sum + sum * percent / 100;
+
+alert("პროცენტი დარიცხვის შემდეგ თანხა იქნება" + sum)
+
+
+var people = ["Tom" , "Alice" , "Sam"]
+
+people[0] = "Bob";
+
+console.log(people[0]) 
+
+
+var numbers1 = [1,2,3,4,5]; //ერთგანზომლიებიან
+
+var numbers2 = [[0,1,2], [3,4,5]] //ორგანზომილებიანი
+
+var people = [
+    ["Tom" , 25 , false], //0
+    ["Alice" , 26 , false], //1
+    ["Sam" , 27 , true] //2
+]
+console.log(people[0][0]) //Tom
+console.log(people[2][2]) //true
+
+
+// if ოპერატორი
+
+// var income = 100; //საათში
+// if (income > 150) {
+//     var message= "კაი ხელფასი"
+//     alert(message)
+// }
+// else{
+//     alert("არ შეიშვება ოჯახში")
+// }
+
+
+// var income = 4001;
+// if (income < 2000) {
+//     alert("საშუალoze dabali")
+// }
+// else if (income >= 2000 && income <=4000) {
+//     alert("sashualo xelpasi")
+// }
+// else {
+//     alert("kai xelpasi")
+// }
+
+
+var income = 200 ; 
+switch(income) {
+    case 100:
+        console.log("საშუალoze dabali")
+        break;
+    case 200 :
+        console.log("sashualo xelpasi")
+        break;
+    case 300 :
+        console.log("kai xelpasi")
+        break;
+}
+
+// var people = ["Tom", "Alice" , "Bolb", "Sam"]
+// console.log(people)
+
+
+// for ([მრიცხველის ინიცილიზაცია], [პრიობა] [მრიცხველის შეცვლა])
+
+var people = ["Tom", "Alice" , "Bolb", "Sam"]
+for(var i = 0; i < people.length; i ++){
+   document.write(people[i] + "</br>")
+}
+// for (ინდექსი in მასივი)
+var people = ["Tom", "Alice" , "Bolb", "Sam"]
+for(var index in people){
+    document.write(people[index])
+}
+
+var arr = [4,5,6,7,15,45,76];
+console.log(arr.reverse())
+
+
 
