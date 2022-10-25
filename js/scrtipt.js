2eff18818f764618a178a704011d2a462975d7eb
Apxo <72552435+G-Apxo@users.noreply.github.com>
2022-10-25 21:15:43 +0400

    t

--- js/scrtipt.js
+++ js/scrtipt.js
@@ -1,24 +1,24 @@
 
 // შეყტობინების გამოტანა
-console.log("გამოთვალე გამოსახულება")
-//არითმეტიკული ოპერაცია
-var a = undefined
+// console.log("გამოთვალე გამოსახულება")
+// //არითმეტიკული ოპერაცია
+// var a = undefined
 
-console.log(a)
+// console.log(a)
 
-var b =  13 + "13" ;
-console.log(b) // 1313
+// var b =  13 + "13" ;
+// console.log(b) // 1313
 
 
-//String სტრიქონს (ტექსტი)
-var companyName = "13"
-console.log(typeof companyName) //stirng
-// number რიცხივითი მნიშვნელობა
-var stundets = 17
-console.log(typeof stundets) //number
-//Boolean  ლოგიკური მნიშვნელობა True ან False
-var isLecture = true;
-console.log(typeof isLecture) //bolean
+// //String სტრიქონს (ტექსტი)
+// var companyName = "13"
+// console.log(typeof companyName) //stirng
+// // number რიცხივითი მნიშვნელობა
+// var stundets = 17
+// console.log(typeof stundets) //number
+// //Boolean  ლოგიკური მნიშვნელობა True ან False
+// var isLecture = true;
+// console.log(typeof isLecture) //bolean
 //undefined მნიშვნელობა არააქვს მინიჭებული
 
 //null  განუსაზღვრელი მნიშვნელობა
@@ -31,96 +31,96 @@ console.log(typeof isLecture) //bolean
 // var y = x + 50; // 60
 
 //გამოკლება
-var x= 10; 
-var y = x - 50;
-console.log(y)
-//გაყოფა
-var x= 10; 
-var y = x / 50;
-console.log(y)
-//გამრავლება
+// var x= 10; 
+// var y = x - 50;
+// console.log(y)
+// //გაყოფა
+// var x= 10; 
+// var y = x / 50;
+// console.log(y)
+// //გამრავლება
 
-var x= 10; 
-var y = x * 50;
-console.log(y)
+// var x= 10; 
+// var y = x * 50;
+// console.log(y)
 
 
-// ნაშთი გაყოფისას
-var x = 43;
-var y = 7 ;
-var z = x % y;
+// // ნაშთი გაყოფისას
+// var x = 43;
+// var y = 7 ;
+// var z = x % y;
 
-console.log(z) // 1
+// console.log(z) // 1
 
-var x = 5;
-var z = x++; // 6
+// var x = 5;
+// var z = x++; // 6
 
 
  
-var income = 100; 
-var strIncome = "100";
+// var income = 100; 
+// var strIncome = "100";
 
-var result1 = income == strIncome;
-var result2 = income === strIncome;
+// var result1 = income == strIncome;
+// var result2 = income === strIncome;
 
-console.log(result1)
-console.log(result2)
+// console.log(result1)
+// console.log(result2)
  
-var income = 100; 
-var percent = 10 ;
-var resutl = income > 50 && percent<8; //და კავშირი
-console.log(resutl) //false
+// var income = 100; 
+// var percent = 10 ;
+// var resutl = income > 50 && percent<8; //და კავშირი
+// console.log(resutl) //false
 
-var income = 100; 
-var isDeposit = true ;
-var resutl = income > 50 || isDeposit ==false; //ან კავშირი
-console.log(resutl)
-
-
-var income = 100;
-var result1 = !(income > 50 );
-console.log (result1) 
+// var income = 100; 
+// var isDeposit = true ;
+// var resutl = income > 50 || isDeposit ==false; //ან კავშირი
+// console.log(resutl)
 
 
-
-var FirstName = "ტომი"
-
-var LastName = "ჯერი" 
-
-var fullname = FirstName + " " + LastName;
-
-console.log(fullname)
+// var income = 100;
+// var result1 = !(income > 50 );
+// console.log (result1) 
 
 
-var strSum = prompt ("შეიყავნეთ ანაბარის თანხა 1000");
-var strPercent = prompt ("შეიყვანეთ საპროცენტო განაკვეთი");
 
-var sum = parseInt(strSum);
-var percent = parseInt(strPercent);
+// var FirstName = "ტომი"
 
-sum = sum + sum * percent / 100;
+// var LastName = "ჯერი" 
 
-alert("პროცენტი დარიცხვის შემდეგ თანხა იქნება" + sum)
+// var fullname = FirstName + " " + LastName;
 
-
-var people = ["Tom" , "Alice" , "Sam"]
-
-people[0] = "Bob";
-
-console.log(people[0]) 
+// console.log(fullname)
 
 
-var numbers1 = [1,2,3,4,5]; //ერთგანზომლიებიან
+// var strSum = prompt ("შეიყავნეთ ანაბარის თანხა 1000");
+// var strPercent = prompt ("შეიყვანეთ საპროცენტო განაკვეთი");
 
-var numbers2 = [[0,1,2], [3,4,5]] //ორგანზომილებიანი
+// var sum = parseInt(strSum);
+// var percent = parseInt(strPercent);
 
-var people = [
-    ["Tom" , 25 , false], //0
-    ["Alice" , 26 , false], //1
-    ["Sam" , 27 , true] //2
-]
-console.log(people[0][0]) //Tom
-console.log(people[2][2]) //true
+// sum = sum + sum * percent / 100;
+
+// alert("პროცენტი დარიცხვის შემდეგ თანხა იქნება" + sum)
+
+
+// var people = ["Tom" , "Alice" , "Sam"]
+
+// people[0] = "Bob";
+
+// console.log(people[0]) 
+
+
+// var numbers1 = [1,2,3,4,5]; //ერთგანზომლიებიან
+
+// var numbers2 = [[0,1,2], [3,4,5]] //ორგანზომილებიანი
+
+// var people = [
+//     ["Tom" , 25 , false], //0
+//     ["Alice" , 26 , false], //1
+//     ["Sam" , 27 , true] //2
+// ]
+// console.log(people[0][0]) //Tom
+// console.log(people[2][2]) //true
 
 
 // if ოპერატორი
@@ -147,18 +147,18 @@ console.log(people[2][2]) //true
 // }
 
 
-var income = 200 ; 
-switch(income) {
-    case 100:
-        console.log("საშუალoze dabali")
-        break;
-    case 200 :
-        console.log("sashualo xelpasi")
-        break;
-    case 300 :
-        console.log("kai xelpasi")
-        break;
-}
+// var income = 200 ; 
+// switch(income) {
+//     case 100:
+//         console.log("საშუალoze dabali")
+//         break;
+//     case 200 :
+//         console.log("sashualo xelpasi")
+//         break;
+//     case 300 :
+//         console.log("kai xelpasi")
+//         break;
+// }
 
 // var people = ["Tom", "Alice" , "Bolb", "Sam"]
 // console.log(people)
@@ -166,18 +166,162 @@ switch(income) {
 
 // for ([მრიცხველის ინიცილიზაცია], [პრიობა] [მრიცხველის შეცვლა])
 
-var people = ["Tom", "Alice" , "Bolb", "Sam"]
-for(var i = 0; i < people.length; i ++){
-   document.write(people[i] + "</br>")
+// var people = ["Tom", "Alice" , "Bolb", "Sam"]
+// for(var i = 0; i < people.length; i ++){
+//    document.write(people[i] + "</br>")
+// }
+// // for (ინდექსი in მასივი)
+// var people = ["Tom", "Alice" , "Bolb", "Sam"]
+// for(var index in people){
+//     document.write(people[index])
+// }
+
+// var arr = [4,5,6,7,15,45,76];
+// console.log(arr.reverse())
+
+
+
+// function ფუნქციის სახელი ([პარამეტრი]) {
+//      ინსტრუქცია
+// }
+
+// function display() {
+//     document.write('es aris js pirveli punqcia')
+//   }
+//   display();
+
+// var display = function () {
+//     document.write('es aris js pirveli punqcia')
+// }
+// display ();
+
+// function goodMorning() {
+//     document.write("dila mshvidobis")
+// }
+// function  goodEvning() {
+//     document.write("sagamo mshvidobis")
+//   }
+
+//   var message = goodMorning();
+
+//   message();
+
+//   message = goodEvning();
+
+//   message();
+
+
+// function display(x) {
+//     var z = x * x; 
+//     document.write(x + z);
+//  };
+// display(5);
+
+
+// function display (x,y) {
+//     if ( y === undefined )
+//     y=x;
+//     var z = x*y;
+//     document.write( x + "გამრვალებული" + y + "უდრის" + z );
+// }
+// // display(6) // 0, undefined, false , 36 , 12 , 
+// display(6,4)
+
+
+// function display() {
+//     var z = 1;
+//     for (var i = 0; i < arguments.length; i++)
+//         z *= arguments[i];
+//     document.write(z);
+// }
+// display(6) // 6
+// display(6,4) // 24
+// display(6,4,5) // 120
+
+
+// var x = 5; 
+
+// function displaySquare () { 
+//     var z = x * x;
+//     document.write(z)
+//  }
+//  displaySquare()
+
+// function displaySquare () {
+//     var z  = 10 ;
+//     console.log(z)
+// }
+// displaySquare()
+// console.log(z) //შეცდომა რადგანაც z არ არსებობს
+
+
+// "use strict";
+// function bar() {
+//     foo = "25";
+// }
+// bar();
+// console.log(foo);
+
+
+// function getFactorial(n) {
+//     if (n === 1) {
+//         return 1;
+//     }
+//     else {
+//         return n * getFactorial( n - 1 );
+//     }
+// }
+// var result = getFactorial(4);
+
+// console.log(result) //24
+
+
+// var resutl = 4 * getFactorial(3)
+// var resutl = 4 * 3 * getFactorial(2)
+// var resutl = 4 * 3 * 2 * getFactorial(1)
+// var resutl = 4 * 3 * 2 * 1 * getFactorial(0) //24 
+
+
+function display(){
+    console.log("დილა მშვიდობისა");
+    display = function(){
+        console.log("დღე მშვიდობისა");
+    }
 }
-// for (ინდექსი in მასივი)
-var people = ["Tom", "Alice" , "Bolb", "Sam"]
-for(var index in people){
-    document.write(people[index])
+
+var displayMessage = display;
+display() // დილა მშვიდობისა
+display() // დღე მშვიდობისა"
+displayMessage() // 
+displayMessage() //
+
+// Hoisting
+
+console.log(foo) // undefined
+
+var foo = "giorrgi"
+
+
+var c = a * b ;
+
+var a = 5;
+
+var b = 3;
+
+console.log(c) // NaN
+
+
+var user = {} ;
+
+user.name = "Tom"
+user.age = 25;
+
+console.log(user)
+var user = {
+    name : "Tom",
+    age : 26,
+    display : function() {
+        console.log(this.name);
+    }
 }
-
-var arr = [4,5,6,7,15,45,76];
-console.log(arr.reverse())
-
-
-
+user.display();
